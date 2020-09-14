ef54554b99f29a3eab8492d3f82a6f156ce00c8d
adammatoszko <adamskey@gmail.com>
2020-09-14 23:37:19 +0200

    attribute on sfmodel

---
+++ SalesforceConnector/Models/SalesforceAttributeModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace SalesforceConnector.Models
+{
+    public readonly struct SalesforceAttributeModel
+    {
+        [DataMember(Name = "type")]
+        public string Type { get; }
+
+        public SalesforceAttributeModel(string objectType)
+        {
+            Type = objectType;
+        }
+    }
+}
--- SalesforceConnector/Models/SalesforceObjectModel.cs
+++ SalesforceConnector/Models/SalesforceObjectModel.cs
@@ -1,7 +1,17 @@
-﻿namespace SalesforceConnector.Models
+﻿using System.Runtime.Serialization;
+
+namespace SalesforceConnector.Models
 {
     public abstract class SalesforceObjectModel
     {
         public string Id { get; set; }
+
+        [DataMember(Name = "attributes")]
+        public SalesforceAttributeModel Attributes { get; }
+
+        public SalesforceObjectModel(string objectType)
+        {
+            Attributes = new SalesforceAttributeModel(objectType);
+        }
     }
 }
